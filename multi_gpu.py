3cd9d823d6d9e1d0f658a92261dbd112aaf1d5d8
Joe <infomrhua@gmail.com>
2021-09-18 14:51:22 -0700

    made some change

--- multi_gpu.py
+++ multi_gpu.py
@@ -52,7 +52,10 @@ class ToyMpModel(nn.Module):
 '''
 
 def demo_basic(rank, world_size):
-    print(f"Running basic DDP example on rank {rank}.")
+    
+    
+    #print(f"Running basic DDP example on rank {rank}.")
+    
     setup(rank, world_size)
 
     # create model and move it to GPU with id rank
@@ -150,10 +153,16 @@ def demo_checkpoint(rank, world_size):
 
 
 if __name__ == "__main__":
+    
     n_gpus = torch.cuda.device_count()
+    
     print('device count:',n_gpus)
     assert n_gpus >= 2, f"Requires at least 2 GPUs to run, but got {n_gpus}"
+
     world_size = n_gpus
+    
     run_demo(demo_basic, world_size)
-    run_demo(demo_checkpoint, world_size)
+    #run_demo(demo_checkpoint, world_size)
+
+
     #run_demo(demo_model_parallel, world_size)
