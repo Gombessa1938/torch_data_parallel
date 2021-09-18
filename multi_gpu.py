c910bbf39a6ff95847a58bc52ba1359e40738abc
Joe <infomrhua@gmail.com>
2021-09-18 14:56:34 -0700

    .

--- multi_gpu.py
+++ multi_gpu.py
@@ -53,7 +53,7 @@ class ToyMpModel(nn.Module):
 
 def demo_basic(rank, world_size):
     
-    
+
     #print(f"Running basic DDP example on rank {rank}.")
     
     setup(rank, world_size)
@@ -78,6 +78,7 @@ def demo_basic(rank, world_size):
 
 
 def run_demo(demo_fn, world_size):
+    
     mp.spawn(demo_fn,
              args=(world_size,),
              nprocs=world_size,
@@ -157,12 +158,13 @@ if __name__ == "__main__":
     n_gpus = torch.cuda.device_count()
     
     print('device count:',n_gpus)
+
     assert n_gpus >= 2, f"Requires at least 2 GPUs to run, but got {n_gpus}"
 
     world_size = n_gpus
     
     run_demo(demo_basic, world_size)
+
+
     #run_demo(demo_checkpoint, world_size)
-
-
     #run_demo(demo_model_parallel, world_size)
